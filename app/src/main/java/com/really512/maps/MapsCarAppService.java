aacc1281aa3e80343ee6ce126437622b49005865
really512 <leshanekrasovot@gmail.com>
2026-09-18 14:34:10 +0300

    fix: connect route state to Android Auto

--- app/src/main/java/com/really512/maps/MapsCarAppService.java
+++ app/src/main/java/com/really512/maps/MapsCarAppService.java
@@ -1,14 +1,17 @@
 package com.really512.maps;
 
+import android.content.pm.ApplicationInfo;
+import androidx.annotation.NonNull;
 import androidx.car.app.CarAppService;
 import androidx.car.app.Session;
+import androidx.car.app.SessionInfo;
 import androidx.car.app.validation.HostValidator;
 
 public class MapsCarAppService extends CarAppService {
-    @Override public HostValidator createHostValidator() {
+    @NonNull @Override public HostValidator createHostValidator() {
         return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR;
     }
-    @Override public Session onCreateSession() {
+    @NonNull @Override public Session onCreateSession(@NonNull SessionInfo sessionInfo) {
         return new MapsCarSession();
     }
 }
